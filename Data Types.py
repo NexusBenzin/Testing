35610f53dd1bf6dd9ef21c7cc666066ff100ee45
Nexus <timotej.petrik1@gmail.com>
2026-03-05 17:43:48 +0100

    Adding Functions and Data Types

---
+++ Data Types.py
@@ -0,0 +1,33 @@
+# Boolean (bool)
+# True or False
+Boolean = True
+Boolean2 = False
+
+
+# String (str)
+# A string of characters
+String = "String"
+String2 = "I have the number 2 and it is True that it is a numeber"
+
+
+# Integer (int)
+# A full number
+Integer = (1)
+Integer2 = (1+1)
+
+
+# Float
+# A number with a decimal value
+Float = (1.1)
+Float2 = (1+0.1)
+
+
+# List
+# A list of strings, integers, floats or booleans
+List = [1,2,3,4,5]
+List2 = ["String",1, 0,1, True]
+
+
+# Dictionary
+# A list with values
+Dictionary = {"Name": "Nexus", "Age": 13, "Adult": True}
