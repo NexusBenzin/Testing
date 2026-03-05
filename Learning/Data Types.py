964efe23980f70f27b93d7fb205e1567250c93ee
Nexus <timotej.petrik1@gmail.com>
2026-03-05 19:23:10 +0100

    Adding recap, functions directory and organizing my stuff

--- Learning/Data Types.py
+++ Learning/Data Types.py
@@ -7,19 +7,19 @@ Boolean2 = False
 # String (str)
 # A string of characters
 String = "String"
-String2 = "I have the number 2 and it is True that it is a numeber"
+String2 = "I have the number 2 and it is True that it is a number"
 
 
 # Integer (int)
 # A full number
-Integer = (1)
-Integer2 = (1+1)
+Integer = 1
+Integer2 = 1+1
 
 
 # Float
 # A number with a decimal value
-Float = (1.1)
-Float2 = (1+0.1)
+Float = 1.1
+Float2 = 1+0.1
 
 
 # List
---
+++ Learning/recap.py
@@ -0,0 +1,54 @@
+# Variables and Data Types
+float = 0.1
+integer = 1
+string = "Hello World"
+boolean = True
+
+list = [1,2,3,4,5]   # ideally should not have miss-matched data types
+
+dictionary = {"a": 1,
+              "b": True,
+              "c": 0.1,
+              "d": "Hello World",
+}
+
+# Conditions
+y = "bbb"
+x = "aaa"
+
+if x == "aaa":
+    print("x equals aaa")
+else:
+    print("x does not equal aaa")
+
+
+if x == "aaa" and y == "bbb":
+    print("x equals aaa and y  equals bbb")
+else:
+    print("either x does not equal aaa or y does not equal bbb")
+
+
+
+# Cycles
+
+books = [{"Name": "Benzin"},
+         {"Name": "Diesel is BLEUGH"}
+]
+
+#for book in books:
+
+
+for i in range(10):
+    print(i)
+
+#time.sleep(5)
+#while True:
+#    print("While True: cycle")
+
+
+# Functions
+
+def sum(a: int, b: int) -> int:
+    return a + b
+
+sum(1, 2)
